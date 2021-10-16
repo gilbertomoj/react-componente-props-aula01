470a594ff58709d12e1be7d46613f0426eddeac2
gilbertomoj <gibamedeirosgc@gmail.com>
2021-10-16 00:40:49 -0300

    Atividade

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Carta from "./Carta";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Olá</h1>
+      <Carta remetente="Joana" destino="Paraná"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
